057996120a6602fe5c1ece5e647ccc931ad4d1e2
humany <yoyo@qq.com>
2023-02-13 18:34:18 +0800

    feat: change config

--- src/gift.setting.js
+++ src/gift.setting.js
@@ -11,15 +11,15 @@ export const IS_SHOW_RESTART = false;
 // 首页配置
 export const homeConfig = {
   // 标题
-  title: '520送礼抽奖',
+  title: '2.14情人节 猪猪抽奖活动',
   // 留言，数组的每一项代表一行
   messages: [
-    { key: 'a', wording: '这是第一行文案', },
-    { key: 's', wording: '这是第二行文案', },
-    { key: 'd', wording: '然后，这里是第三行文案', },
+    { key: 'a', wording: '嘿，猪猪，这个网站是为你订做的', },
+    { key: 's', wording: '我也不知道该给我家猪猪买什么礼物，那就让猪猪自己选吧', },
+    { key: 'd', wording: '选到啥就是啥 哈哈', },
   ],
   // 最终解释权归属人
-  owner: 'XXX',
+  owner: 'YHL',
   // 抽奖转盘的动画时间，单位毫秒
   timeout: 5000,
   // 指定赠送的礼物的索引（第几个礼物中奖），取值范围 0-8
@@ -37,6 +37,6 @@ export const gifts = [
   { key: 'r', name: '520红包', alias: '520', image: '/images/6.png',  description: '“想给你唱一百首情歌”' },
   { key: 't', name: 'Dior星空套装', alias: 'Dior星空', image: '/images/5.png',  description: '「百变唇妆，精美雕琢」' },
   { key: 'y', name: '1314红包', alias: '1314', image: '/images/6.png',  description: '“从今往后，我都会在你旁边”' },
-  { key: 'u', name: 'UR购物券：¥1,000', alias: 'UR', image: '/images/7.png',  description: '“UR！买！”' },
-  { key: 'i', name: 'Dyson美发套装', alias: '戴森', image: '/images/8.png',  description: '「不同造型需求，全面满足」' },
+  { key: 'u', name: '小米13', alias: 'mi13', image: '/images/7.jpeg',  description: '“你值得拥有”' },
+  { key: 'i', name: '花架', alias: '花架', image: '/images/8.jpeg',  description: '「点点滴滴都在心里」' },
 ];
